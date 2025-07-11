e639c698e3cc6a172ffa5cb42ab05d4b5d5821aa
senasimsekk <c2311066@student.cankaya.edu.tr>
2025-07-11 11:10:14 +0300

    tohumun manuel ekilmesi

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,39 +1,44 @@
 'use client';
 
+import { useState } from 'react';
 import styles from './Game.module.css';
 
 export default function GamePage() {
-  const handleClick = () => {
-    alert('tıklama çalıştı');
-    // Burada kartın işlevselliğini ekleyebilirsiniz
-    // Örneğin, kartın durumunu güncelleme veya başka bir işlem yapma
-    
-  };
+  const [fields, setFields] = useState<string[]>(Array(16).fill('e')); 
+
+ const handleClick = (index: number) => {
+  setFields((prev) => {
+    const updated = [...prev];
+    const current = updated[index];
+
+    if (current === 'e') updated[index] = 't';
+    else if (current === 't') updated[index] = 'f';
+    else if (current === 'f') updated[index] = 'b';
+    else if (current === 'b') updated[index] = 'ç';
+    else if (current === 'ç') updated[index] = 'k';
+    else if (current === 'k') updated[index] = 'e';
+
+    return updated;
+  });
+};
+
+  
+
 
   return (
     <div className={styles.container}>
       
 
       <div className={styles.grid}>
-        <button className={styles.card} onClick={handleClick}></button>
-        <button className={styles.card} onClick={handleClick}></button>
-        <button className={styles.card} onClick={handleClick}></button>
-        <button className={styles.card} onClick={handleClick}></button>
-
-        <button className={styles.card} onClick={handleClick}></button>
-        <button className={styles.card} onClick={handleClick}></button>
-        <button className={styles.card} onClick={handleClick}></button>
-        <button className={styles.card} onClick={handleClick}></button>
-
-        <button className={styles.card} onClick={handleClick}></button>
-        <button className={styles.card} onClick={handleClick}></button>
-        <button className={styles.card} onClick={handleClick}></button>
-        <button className={styles.card} onClick={handleClick}></button>
-
-        <button className={styles.card} onClick={handleClick}></button>
-        <button className={styles.card} onClick={handleClick}></button>
-        <button className={styles.card} onClick={handleClick}></button>
-        <button className={styles.card} onClick={handleClick}></button>
+        {fields.map((val, index) => (
+          <button
+            key={index}
+            className={styles.card}
+            onClick={() => handleClick(index)}
+          >
+            {val}
+          </button>
+        ))}
       </div>
     </div>
   );
